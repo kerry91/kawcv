d12cfeb82814f3bcd33459cbb6c813d40f3b5638
KAW <36702039+kerry91@users.noreply.github.com>
2022-11-29 11:48:59 +0000

    .

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,3 +1,5 @@
+import me from '../images/me.jpg'
+
 const Profile = () => {
     return (
       <>
@@ -6,7 +8,7 @@ const Profile = () => {
             <div class="card">
               <img
               className="profile-picture"
-                src="https://images.unsplash.com/photo-1581090700227-1e37b190418e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                src={me}
                 alt="Profile"
               />
               <h3>Kerry A Warnes</h3>
